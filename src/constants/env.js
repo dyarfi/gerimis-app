6549310def3d647940ad9fdb1bac4cf4bd6348b4
defrian.yarfi@gmail.com <defrian.yarfi@gmail.com>
2021-09-25 22:57:23 +0700

    chore: Add files

---
+++ src/constants/env.js
@@ -0,0 +1,8 @@
+export const API_OMAP_BASE = 'https://openweathermap.org/'
+export const API_GMAP_BASE = 'https://www.google.com/'
+
+export const API_OMAP_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+export const API_GMAP_URL = 'https://www.google.com/maps/embed/v1/place?'
+
+export const API_OMAP_KEY = 'fdf871cedaf3413c6a23230372c30a02'
+export const API_GMAP_KEY = 'AIzaSyCjcYHhFOEAmnQyRtt72xwzC6MdqYhDdQU'
