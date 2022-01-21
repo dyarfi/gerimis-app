d7f2e917522aaf2a885d46f998e5120b97a6bdff
defrian.yarfi@gmail.com <defrian.yarfi@gmail.com>
2022-01-21 22:18:10 +0700

    chore: update offline handler

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app" class="main-app">
-    <router-view v-if="!errors" />
+    <router-view v-if="!errors && onLine" />
     <div
       v-else
       class="flex items-center justify-center h-screen xl:w-1/2 mx-auto px-6"
     >
       <h1 class="text-3xl md:text-6xl md:font-bold text-center">
-        {{ errors }}
-        <small class="text-lg md:text-xl block mt-4"
+        <span class="hidden">{{ errors }}</span>
+        <small class="text-lg md:text-3xl block mt-4"
           >Please re-check your internet or your browser permission to
           continue.</small
         >
+        <!-- {{ navigator.onLine }} -->
       </h1>
+    </div>
+    <div
+      v-if="!onLine"
+      class="border rounded-md w-1/3 mx-auto mb-2 py-1 px-2 fixed top-2 right-2 text-md"
+    >
+      you are offline...
+    </div>
+    <div
+      v-if="showBackOnline"
+      class="border rounded-md w-1/3 mx-auto mb-2 py-1 px-2 fixed top-2 right-2 text-md"
+    >
+      back online...
     </div>
   </div>
 </template>
@@ -26,7 +39,9 @@ export default {
       locationLatLong: {},
       getLocation: false,
       cityName: '',
-      errors: null
+      errors: null,
+      onLine: navigator.onLine,
+      showBackOnline: false
     }
   },
   methods: {
@@ -54,12 +69,33 @@ export default {
     },
     setErrors(errors) {
       this.errors = errors
+    },
+    updateOnlineStatus(e) {
+      const { type } = e
+      this.onLine = type === 'online'
     }
   },
   created() {
     const vm = this
     vm.getWeatherByLocation()
-    // vm.$store.dispatch('setTemp', vm.$store.state.setup.temp)
+  },
+  watch: {
+    onLine(v) {
+      if (v) {
+        this.showBackOnline = true
+        setTimeout(() => {
+          this.showBackOnline = false
+        }, 1000)
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('online', this.updateOnlineStatus)
+    window.addEventListener('offline', this.updateOnlineStatus)
+  },
+  beforeDestroy() {
+    window.removeEventListener('online', this.updateOnlineStatus)
+    window.removeEventListener('offline', this.updateOnlineStatus)
   },
   computed: {
     ...mapState({
